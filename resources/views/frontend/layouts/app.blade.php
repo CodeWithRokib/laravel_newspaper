364db907adab2671c5ec489b4acbaf28f4a8c863
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-06-07 12:50:51 +0600

    frontend add

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    @include('frontend.layouts.css')
+</head>
+
+<body>
+    @include('frontend.layouts.header')
+
+    @yield('content')
+
+    @include('frontend.layouts.footer')
+    @include('frontend.layouts.js')
+</body>
+
+</html>
---
+++ resources/views/frontend/layouts/css.blade.php
@@ -0,0 +1,11 @@
+
+  <!-- Libraries Stylesheet -->
+
+ <link rel="stylesheet" href="{{asset('lib/animate/animate.min.css')}}">
+ <link rel="stylesheet" href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}">
+
+  <!-- Customized Bootstrap Stylesheet -->
+ <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+
+  <!-- Template Stylesheet -->
+ <link rel="stylesheet" href="{{asset('css/style.css')}}">
